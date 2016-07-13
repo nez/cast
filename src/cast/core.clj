b28803cebb6802973c62b18d0b11b267d90248d6
Andres Gomez Urquiza <andres@fractal.ai>
2016-07-12 23:21:17 -0500

    guid and changes in widget structr

--- src/cast/core.clj
+++ src/cast/core.clj
@@ -3,30 +3,59 @@
             [clojure.string :as s]
             [mongerr.core :refer :all]))
 
-(defn dash
-  [id]
-  (:data (db-findf :dash {:id id})))
+(defmulti dash number?)
 
-(defn dash-update
-  [id newdata]
-  (db-update :dash
-             {:id id}
-             {:data newdata}))
+(defmethod dash true [id] (:data (db-findf :dash {:id id})))
+
+(defmethod dash false [id] (dash (read-string id)))
+
+(defmulti dash-update (fn [id newdata] (number? id)))
+
+(defmethod dash-update false [id newdata]
+  (dash-update (read-string id)
+               newdata))
+
+(defmethod dash-update true [id newdata]
+  (db-upsert :dash {:id id} (assoc  {:data newdata} :id id)))
+
+(defn uuid [] (str (java.util.UUID/randomUUID)))
+
+(defn widget [dash-id uuid]
+  (first (filter #(= uuid (:uuid %))
+                 (dash dash-id))))
 
 (defn new-widget
   [id widget]
-  (dash-update id (conj (dash id) widget)))
+  (dash-update id
+               (conj (dash id)
+                     (assoc widget :uuid (uuid)))))
 
 (defn edit-widget
   [id widget]
-  (dash-update id
-               (map #(if (= (:name widget) ;TODO if you change the name then it doesnt update
-                            (:name %))
-                       widget
-                       %)
-                    (dash id))))
+  (let [il-dash (dash id)
+        dash-update (map #(if (= (:uuid widget) ;TODO if you change the name then it don update
+                                 (:uuid %))
+                            widget
+                            %)
+                         il-dash)]
+    (dash-update id
+                 (if (= il-dash dash-update)
+                   (conj il-dash (assoc widget :uuid (uuid)))
+                   dash-update))))
 
 (defn delete-widget
-  [id name]
-  (dash-update id (remove #(= name (:name %))
+  [id uuid]
+  (dash-update id (remove #(= uuid (:uuid %))
                           (dash id))))
+
+(defn update-widget-data [widget]
+  (try (assoc widget :data (eval (read-string (:dataexpr widget))))
+       (catch Exception e widget)))
+
+(defmulti update-dash-data [id] number?)
+
+(defmethod update-dash-data true [id]
+  (dash-update id (map update-widget-data (dash-id))))
+
+(defmethod update-dash-data false [id]
+  (update-dash-data (read-string id)))
